27316d0f0bcd0d7f26bf5e1b29d5fdabd30ff4ba
Skivel <denisbuhrov@gmail.com>
2023-02-26 16:13:41 +0100

    instruction

--- Tutorial_Django_REST_API/api.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from items import api_views as items_views
-
-
-router = routers.DefaultRouter()
-router.register(r'friends', items_views.FriendViewset)
-router.register(r'belongings', items_views.BelongingViewset)
-router.register(r'borrowings', items_views.BorrowedViewSet)
